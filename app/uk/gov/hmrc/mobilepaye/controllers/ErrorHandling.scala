e2ad78ec58834f945e274f2f834fe7b63641f04d
Martin Rolfe <52818407+mrolfe88@users.noreply.github.com>
2021-06-17 13:47:08 +0100

    [HMA-4527] Revert changes

--- app/uk/gov/hmrc/mobilepaye/controllers/ErrorHandling.scala
+++ app/uk/gov/hmrc/mobilepaye/controllers/ErrorHandling.scala
@@ -36,6 +36,9 @@ case object ErrorUnauthorizedUpstream
 
 case object ErrorBadRequest extends ErrorResponse(400, "BAD_REQUEST", "Invalid POST request")
 
+case object LockedUserRequest
+  extends ErrorResponse(423, "LOCKED", "Locked! User is locked due to manual correspondence indicator flag being set")
+
 case object MandatoryResponse extends ErrorResponse(500, "MANDATORY", "Mandatory data not found")
 
 case object ForbiddenAccess extends ErrorResponse(403, "UNAUTHORIZED", "Access denied!")
@@ -70,6 +73,10 @@ trait ErrorHandling {
       case ex: Upstream4xxResponse if ex.upstreamResponseCode == 401 =>
         log("Upstream service returned 401")
         Status(ErrorUnauthorizedUpstream.httpStatusCode)(toJson(ErrorUnauthorizedUpstream))
+
+      case ex: Upstream4xxResponse if ex.upstreamResponseCode == 423 =>
+        log("Locked! User is locked due to manual correspondence indicator flag being set")
+        Status(LockedUserRequest.httpStatusCode)(toJson(LockedUserRequest))
 
       case _: AuthorisationException =>
         log("Unauthorised! Failure to authorise account or grant access")
