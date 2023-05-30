db3611c9d39089465fc85478ab34d879b9cab28c
Martin Rolfe <52818407+mrolfe88@users.noreply.github.com>
2023-05-30 15:41:17 +0100

    [HMA-6938] Add tax code change to prod test account (#127)

--- it/SandboxMobilePayeControllerISpec.scala
+++ it/SandboxMobilePayeControllerISpec.scala
@@ -26,6 +26,7 @@ class SandboxMobilePayeControllerISpec extends BaseISpec {
       (response.json \\ "otherIncomes").isEmpty                                       shouldBe false
       (response.json \ "taxFreeAmount").as[Int]                                       shouldBe 12500
       (response.json \ "estimatedTaxAmount").as[Int]                                  shouldBe 1578
+      (response.json \ "taxCodeChange" \ "hasChanged").as[Boolean]                    shouldBe true
       (response.json \ "employments" \ 0 \ "latestPayment" \ "amount").as[BigDecimal] shouldBe BigDecimal(1575)
       (response.json \ "employments" \ 0 \ "latestPayment" \ "date")
         .as[String] shouldBe LocalDate.now().plusDays(2).toString
